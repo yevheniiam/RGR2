de3332494e752522f846c2c71a741e15a08ae64d
yevheniiam <yevheniia.moiseieva@student.karazin.ua>
2024-05-13 00:53:22 +0300

    comm

---
+++ src/mybeans/data_sheet_tablet/DataSheetChangeListener.java
@@ -0,0 +1,7 @@
+package mybeans.data_sheet_tablet;
+
+import java.util.EventListener;
+
+public interface DataSheetChangeListener extends EventListener {
+    public void dataChanged(DataSheetChangeEvent e);
+}
